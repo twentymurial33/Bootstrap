4692f40ca0fc06f7b11d0974b34fbfc0cb042dfb
Murial Anindo <twentymurial@outlook.com>
2018-06-20 14:43:40 -0400

    changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(express.static('assets'));
 
 // require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
